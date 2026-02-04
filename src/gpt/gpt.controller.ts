580b4986e03c88ec678c5f0b4ff09f108d6e752f
Victor <v.mellaq@gmail.com>
2026-02-04 12:37:14 -0300

    Agregar soporte para el uso de pros y contras con OpenAI, incluyendo DTOs, controladores y casos de uso

--- src/gpt/gpt.controller.ts
+++ src/gpt/gpt.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { GptService } from './gpt.service';
-import { OrthographyDto } from './dto';
+import { OrthographyDto, ProsConsDiscusserDto } from './dto';
+import { Response } from 'express';
 
 @Controller('gpt')
 export class GptController {
@@ -10,5 +18,32 @@ export class GptController {
   @HttpCode(200)
   async orthographyCheck(@Body() orthographyDto: OrthographyDto) {
     return this.gptService.orthographyCheck(orthographyDto);
+  }
+
+  @Post('pros-cons-discusser')
+  @HttpCode(200)
+  async prosConsDicusser(@Body() prosConsDiscusserDto: ProsConsDiscusserDto) {
+    return this.gptService.prosConsDicusser(prosConsDiscusserDto);
+  }
+
+  @Post('pros-cons-discusser-stream')
+  async prosConsDicusserStream(
+    @Body() prosConsDiscusserDto: ProsConsDiscusserDto,
+    @Res() res: Response,
+  ) {
+    const stream = await this.gptService.prosConsDicusserStream(
+      prosConsDiscusserDto,
+    );
+
+    res.setHeader('Content-Type', 'application/json');
+    res.status(HttpStatus.OK);
+
+    for await (const chunk of stream) {
+      const piece = chunk.choices[0].delta.content || '';
+      // console.log(piece);
+      res.write(piece);
+    }
+
+    res.end();
   }
 }
--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,16 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import OpenAI from 'openai';
-import { OrthographyDto } from './dto';
-import { orthographyCheckUseCase } from './use-case';
+import { OrthographyDto, ProsConsDiscusserDto } from './dto';
+import {
+  orthographyCheckUseCase,
+  prosConsDicusserStreamUseCase,
+  prosConsDicusserUseCase,
+} from './use-case';
 @Injectable()
 export class GptService {
   private openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
   });
 
-  async orthographyCheck(orthographyDto: OrthographyDto) {
+  async orthographyCheck({ prompt }: OrthographyDto) {
     return await orthographyCheckUseCase(this.openai, {
-      prompt: orthographyDto.prompt,
+      prompt,
     });
+  }
+
+  async prosConsDicusser({ prompt }: ProsConsDiscusserDto) {
+    return await prosConsDicusserUseCase(this.openai, {
+      prompt,
+    });
+  }
+
+  async prosConsDicusserStream({ prompt }: ProsConsDiscusserDto) {
+    return await prosConsDicusserStreamUseCase(this.openai, { prompt });
   }
 }
